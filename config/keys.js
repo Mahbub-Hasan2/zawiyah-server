26c2731b00aab400069d69c3675b501d5313c631
Mahbub-Hasan2 <jiboner.adarsho@gmail.com>
2022-03-27 22:36:38 +0600

    new commit

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+// const key = {
+//     mongoURI:"mongodb+srv://admin:zawiyahAdmin20@wellness.yuu5d.mongodb.net/zawiyahDB?retryWrites=true&w=majority"
+// }
+const key = {
+    mongoURI:"mongodb+srv://admin:zawiyahAdmin20@wellness.yuu5d.mongodb.net/zawiyahDB-prod?retryWrites=true&w=majority"
+}
+export default  key;
---
+++ controllers/user.js
@@ -0,0 +1,136 @@
+import user from '../models/user.js'
+import { isAdmin } from './admin.js'
+import bcrypt from "bcrypt"
+
+/*
+NOTE: 
+-> use newUser.generateHash(newUser.password) to encrypt the user password
+    This function returns the encrypted password
+-> use newUser.validPassword(req.body.password) to validate user password
+    This function return either true or false
+*/
+
+export const createUser = async (req, res) => {
+    const us = req.body;
+    const newUser = user(us);
+    newUser.password = newUser.generateHash(us.password)
+
+    try {
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (err) {
+        res.status(409).json({ message: err.message });
+    }
+}
+
+// export const updateUser = async (req, res)=>{
+//     console.log("update user function")    
+//     const us = req.body;
+//     const id = us._id
+//     var query = {'_id': id};
+//     const newUser = user(us);
+//     newUser.password = newUser.generateHash(us.password)
+//     try{
+//         await user.findOneAndUpdate(query, newUser, {upsert : true});
+//         res.status(201).json(newUser);
+//     } catch(err){
+//         res.status(409).json({message: err.message});
+//     }
+// }
+
+
+// new code start mahbub ==================
+
+export const updateUser = async (req, res) => {
+    console.log("update user function")
+    const us = req.body;
+    const id = us._id
+    var query = { '_id': id };
+    if (us.password) {
+
+        const data = bcrypt.compareSync(us.currentUserPassword, us.currentPassword);
+        console.log(data)
+        if (data) {
+            const newUser = user(us);
+            newUser.password = newUser.generateHash(us.password)
+            try {
+                await user.findOneAndUpdate(query, newUser, { upsert: true });
+                res.status(201).json(newUser);
+            } catch (err) {
+                res.status(409).json({ message: err.message });
+            }
+        }
+        else{
+            res.status(409).json({ message: 'your password not Match' });
+        }
+
+    }
+    else {
+        const newUser = user(us);
+        try {
+            await user.findOneAndUpdate(query, newUser, { upsert: true });
+            res.status(201).json(newUser);
+        } catch (err) {
+            res.status(409).json({ message: err.message });
+        }
+    }
+}
+
+// new code end mahbub ============================
+
+export const getUsers = async (req, res) => {
+    try {
+        const allUser = await user.find();
+        res.status(200).json(allUser);
+    }
+    catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+export const loginUser = async (req, res) => {
+    // console.log(res.body);
+    if ('username' in req.body) {
+        if (await isAdmin(req.body)) {
+            res.status(200).json({ message: "admin logged in" });
+        }
+        else {
+            res.status(404).json({ message: "admin not found" });
+        }
+    }
+    else {
+        user.findOne({ email: req.body.email }, (err, us) => {
+            if (us) {
+                if (!us.validPassword(req.body.password)) {
+                    res.status(404).json({ message: "failed" });
+                } else {
+                    res.status(200).json({ message: "success", user: us });
+                }
+            }
+            else {
+                res.status(404).json({ message: "failed" });
+            }
+        });
+    }
+}
+export const getUser = async (req, res) => {
+    const id = req.body.id;
+    console.log("getting single user", id)
+    try {
+        const us = await user.findById(id);
+        res.status(200).json(us);
+    }
+    catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    const id = req.body._id;
+    console.log(id)
+    try {
+        await user.find({ _id: id }).deleteOne();
+        res.status(201).json({ message: "successfully deleted" });
+    } catch (err) {
+        res.status(409).json({ message: err.message });
+    }
+}
